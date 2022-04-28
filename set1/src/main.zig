d224294aeffe90d29522b1888235fedc1c17b49a
Tim Kieras <tim@tmba.local>
2022-04-27 22:20:45 -0400

    Add full hex string to b64 encoding support

--- set1/src/main.zig
+++ set1/src/main.zig
@@ -1,28 +1,240 @@
 const std = @import("std");
 
-const base_64_alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/";
+const base_64_alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/=";
 
 pub fn main() anyerror!void {
-    const bytes = [3]u8{ 0x49, 0x27, 0x6d };
-    three_bytes_to_base_64(bytes);
+    var arena = std.heap.ArenaAllocator.init(std.heap.page_allocator);
+    defer arena.deinit();
+    const allocator = arena.allocator();
+
+    //const example = "49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d";
+
+    const example = "4d6";
+    const bytes = try hex_string_to_bytes(allocator, example[0..]);
+
+    const sextets = try octets_to_sextets(allocator, bytes);
+
+    const out = std.io.getStdOut();
+    var w = out.writer();
+
+    for (sextets) |sextet| {
+        try w.print("{c}", .{base_64_alphabet[sextet]});
+    }
+}
+pub fn hex_string_to_bytes(allocator: std.mem.Allocator, hex_str: []const u8) ![]u8 {
+    var out_length: usize = @divFloor(hex_str.len, 2);
+
+    if (hex_str.len % 2 != 0) {
+        out_length += 1;
+    }
+    const out_buffer = try allocator.alloc(u8, out_length);
+
+    var char_val: u8 = 0;
+    var pos: u8 = 0;
+    var buffer: u8 = 0;
+    var out_pos: u8 = 0;
+
+    for (hex_str) |char| {
+        if (char <= '9' and char >= '0') {
+            char_val = char - '0';
+        } else if (char <= 'Z' and char >= 'A') {
+            char_val = 10 + (char - 'A');
+        } else if (char <= 'z' and char >= 'a') {
+            char_val = 10 + (char - 'a');
+        }
+
+        std.log.debug("char_val: {}", .{char_val});
+        if (pos == 0) {
+            buffer = char_val << 4;
+        } else if (pos == 1) {
+            buffer = buffer | (char_val & 15);
+            std.log.debug("byte: {x}", .{buffer});
+            out_buffer[out_pos] = buffer;
+            out_pos += 1;
+        }
+
+        pos = (pos + 1) % 2;
+    }
+    if (pos != 0) {
+        std.log.debug("odd number of digits in hex string, left padding final digit with 0", .{});
+        buffer = buffer >> 4;
+        std.log.debug("byte: {x}", .{buffer});
+        out_buffer[out_pos] = buffer;
+    }
+    return out_buffer;
 }
 
-pub fn three_bytes_to_base_64(bytes: [3]u8) void {
-    //    const nums = [4]u8{ 0, bytes[0], bytes[1], bytes[2] };
+pub fn octets_to_sextets(allocator: std.mem.Allocator, bytes: []const u8) ![]u8 {
+    var out_length: usize = @divFloor(bytes.len, 3);
 
-    const num = @byteSwap(u32, @bitCast(u32, [4]u8{ 0, bytes[0], bytes[1], bytes[2] }));
+    if (bytes.len % 3 != 0) {
+        out_length += 1;
+    }
+    out_length *= 4;
 
-    std.log.debug("in: {b}", .{num});
+    const out_buffer = try allocator.alloc(u8, out_length);
 
-    const shifts = [4]u5{ 18, 12, 6, 0 };
+    var pos: u8 = 0;
+    var buffer: u8 = 0;
+    const rshifts = [_]u3{ 2, 4, 6 };
+    const lshifts = [_]u3{ 4, 2, 0 };
+    const masks = [_]u8{ 3, 15, 63 };
+    var sextet_val: u8 = 0;
+    var out_pos: usize = 0;
 
-    for (shifts) |shift_val| {
-        const sextet_val = num >> shift_val & 63;
+    for (bytes) |byte_val| {
+        sextet_val = (buffer | (byte_val >> rshifts[pos]));
+        buffer = (byte_val & masks[pos]) << lshifts[pos];
+        out_buffer[out_pos] = sextet_val;
+        std.log.debug("sextet_val: {c}", .{base_64_alphabet[sextet_val]});
 
-        std.log.debug("out: {c}", .{base_64_alphabet[sextet_val]});
+        if (pos == 2) {
+            out_pos += 1;
+            out_buffer[out_pos] = buffer;
+            buffer = 0;
+        }
+
+        out_pos += 1;
+
+        pos = (pos + 1) % 3;
+    }
+
+    if (pos != 0 and bytes.len != 0) {
+        std.log.debug("sextet_val: {c}", .{base_64_alphabet[buffer]});
+        out_buffer[out_pos] = buffer;
+        out_pos += 1;
+    }
+
+    while (pos != 0 and pos < 3) {
+        std.log.debug("sextet_val: {c}", .{base_64_alphabet[64]});
+        out_buffer[out_pos] = 64;
+        out_pos += 1;
+        pos += 1;
+    }
+    return out_buffer;
+}
+
+test "Cryptopals Test" {
+    var arena = std.heap.ArenaAllocator.init(std.heap.page_allocator);
+    defer arena.deinit();
+    const allocator = arena.allocator();
+
+    const input = "49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d";
+    const expected = "SSdtIGtpbGxpbmcgeW91ciBicmFpbiBsaWtlIGEgcG9pc29ub3VzIG11c2hyb29t";
+
+    const bytes = try hex_string_to_bytes(allocator, input[0..]);
+
+    const sextets = try octets_to_sextets(allocator, bytes);
+
+    try std.testing.expectEqual(expected.len, sextets.len);
+    for (sextets) |sextet, i| {
+        try std.testing.expectEqual(base_64_alphabet[sextet], expected[i]);
     }
 }
 
-test "Wikipedia Test" {
-    try std.testing.expectEqual(10, 3 + 7);
+test "Wikipedia Test No Pad" {
+    var arena = std.heap.ArenaAllocator.init(std.heap.page_allocator);
+    defer arena.deinit();
+    const allocator = arena.allocator();
+
+    const input = "4d616e";
+    const expected = "TWFu";
+
+    const bytes = try hex_string_to_bytes(allocator, input[0..]);
+
+    const sextets = try octets_to_sextets(allocator, bytes);
+
+    try std.testing.expectEqual(expected.len, sextets.len);
+    for (sextets) |sextet, i| {
+        try std.testing.expectEqual(base_64_alphabet[sextet], expected[i]);
+    }
+}
+
+test "Wikipedia Test One Pad" {
+    var arena = std.heap.ArenaAllocator.init(std.heap.page_allocator);
+    defer arena.deinit();
+    const allocator = arena.allocator();
+
+    const input = "4d61";
+    const expected = "TWE=";
+
+    const bytes = try hex_string_to_bytes(allocator, input[0..]);
+
+    const sextets = try octets_to_sextets(allocator, bytes);
+
+    try std.testing.expectEqual(expected.len, sextets.len);
+    for (sextets) |sextet, i| {
+        try std.testing.expectEqual(base_64_alphabet[sextet], expected[i]);
+    }
+}
+test "Wikipedia Test Two Pad" {
+    var arena = std.heap.ArenaAllocator.init(std.heap.page_allocator);
+    defer arena.deinit();
+    const allocator = arena.allocator();
+
+    const input = "4d";
+    const expected = "TQ==";
+
+    const bytes = try hex_string_to_bytes(allocator, input[0..]);
+
+    const sextets = try octets_to_sextets(allocator, bytes);
+
+    try std.testing.expectEqual(expected.len, sextets.len);
+    for (sextets) |sextet, i| {
+        try std.testing.expectEqual(base_64_alphabet[sextet], expected[i]);
+    }
+}
+
+test "Wikipedia Test Repeat" {
+    var arena = std.heap.ArenaAllocator.init(std.heap.page_allocator);
+    defer arena.deinit();
+    const allocator = arena.allocator();
+
+    const input = "4d616e4d616e";
+    const expected = "TWFuTWFu";
+
+    const bytes = try hex_string_to_bytes(allocator, input[0..]);
+
+    const sextets = try octets_to_sextets(allocator, bytes);
+
+    try std.testing.expectEqual(expected.len, sextets.len);
+    for (sextets) |sextet, i| {
+        try std.testing.expectEqual(base_64_alphabet[sextet], expected[i]);
+    }
+}
+
+test "Wikipedia Test Partial Repeat" {
+    var arena = std.heap.ArenaAllocator.init(std.heap.page_allocator);
+    defer arena.deinit();
+    const allocator = arena.allocator();
+
+    const input = "4d616e4d";
+    const expected = "TWFuTQ==";
+
+    const bytes = try hex_string_to_bytes(allocator, input[0..]);
+
+    const sextets = try octets_to_sextets(allocator, bytes);
+
+    try std.testing.expectEqual(expected.len, sextets.len);
+    for (sextets) |sextet, i| {
+        try std.testing.expectEqual(base_64_alphabet[sextet], expected[i]);
+    }
+}
+
+test "Odd Input Length Hex" {
+    var arena = std.heap.ArenaAllocator.init(std.heap.page_allocator);
+    defer arena.deinit();
+    const allocator = arena.allocator();
+
+    const input = "4d6";
+    const expected = "TQY=";
+
+    const bytes = try hex_string_to_bytes(allocator, input[0..]);
+
+    const sextets = try octets_to_sextets(allocator, bytes);
+
+    try std.testing.expectEqual(expected.len, sextets.len);
+    for (sextets) |sextet, i| {
+        try std.testing.expectEqual(base_64_alphabet[sextet], expected[i]);
+    }
 }
